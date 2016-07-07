1b6a53eeb429b837c572ab34e675a404b0bd0248
SegFault42 <rabougue@student.42.fr>
2016-07-07 07:03:33 +0200

    add make run, and other test

--- first_app/source/main.c
+++ first_app/source/main.c
@@ -1,35 +1,49 @@
-#include <string.h>
+/*
+    Both Screen Text example made by Aurelio Mannara for ctrulib
+    This code was modified for the last time on: 12/12/2014 23:20 UTC+1
+*/
 #include <3ds.h>
-
-int main(void)
+#include <stdio.h>
+int main(int argc, char **argv)
 {
-	gfxInitDefault();
-	//gfxSet3D(true); // uncomment if using stereoscopic 3D
-
-	// Main loop
-	while (aptMainLoop())
+	PrintConsole	topScreen;
+	PrintConsole	bottomScreen;
+    // Initialize services
+    gfxInitDefault();
+    //In this example we need one PrintConsole for each screen
+    //Initialize console for both screen using the two different PrintConsole we have defined
+    consoleInit(GFX_TOP, &topScreen);
+    consoleInit(GFX_BOTTOM, &bottomScreen);
+    //Before doing any text printing we should select the PrintConsole in which we are willing to write, otherwise the library will print on the last selected/initialized one
+    //Let's start by printing something on the top screen
+    consoleSelect(&topScreen);
+    printf("\033[32;01mThis is the top screen. We have lot of space here!\n\033[00m\n");
+	for (int i = 0; i < 100; i++)
 	{
-		gspWaitForVBlank();
-		hidScanInput();
-		// Your code goes here
-		u32 kDown = hidKeysDown();
-		if (kDown & KEY_START)
-			break; // break in order to return to hbmenu
-		// Example rendering code that displays a white pixel
-		// Please note that the 3DS screens are sideways (thus 240x400 and 240x320)
-		u8* fb = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL);
-		if (kDown & KEY_A)
-		{
-			fb[3*(10+10*240)] = 0xFF;
-			fb[3*(10+10*240)+1] = 0xFF;
-			fb[3*(10+10*240)+2] = 0xFF;
-		}
-		if (kDown & KEY_SELECT)
-			memset(fb, 0, 240*400*3);
-		// Flush and swap framebuffers
-		gfxFlushBuffers();
-		gfxSwapBuffers();
+		printf("%d\n", i);
 	}
-	gfxExit();
-	return 0;
+    printf("\033[32;01mThis is the top screen. We have lot of space here!\n\033[00m\n");
+    //Now write something else on the bottom screen
+    consoleSelect(&bottomScreen);
+    printf("This is the bottom screen. We don't have much space...\nStill better than DS screens :D");
+    consoleSelect(&topScreen);
+    printf("\033[29;15HPress Start to exit.");
+    // Main loop
+    while (aptMainLoop())
+    {
+        //Scan all the inputs. This should be done once for each frame
+        hidScanInput();
+        //hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
+        u32 kDown = hidKeysDown();
+        if (kDown & KEY_START) break; // break in order to return to hbmenu
+        // Flush and swap framebuffers
+        gfxFlushBuffers();
+        gfxSwapBuffers();
+        //Wait for VBlank
+        gspWaitForVBlank();
+    }
+    // Exit services
+    gfxExit();
+    
+    return 0;
 }
